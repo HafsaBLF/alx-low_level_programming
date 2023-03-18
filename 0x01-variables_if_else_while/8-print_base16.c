a92aca8e907d8e98c059812fc31b6efcc1830970
HafsaBLF <Hafsablf@gmail.com>
2023-03-17 18:04:49 -0700

    ADD

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x[10000];
+int c;
+for (c = 0; c <= 16; c++)
+{
+putchar(x[c]);
+}
+putchar('\n');
+return (0);
+}
