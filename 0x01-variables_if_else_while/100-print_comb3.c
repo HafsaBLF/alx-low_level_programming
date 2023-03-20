778439f1960c2d9e7c5b77ec6714c9c33f2d27f2
HafsaBLF <Hafsablf@gmail.com>
2023-03-20 15:36:03 -0700

    ADD

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x, y, z;
+for (x = 0; x <= 100; x++)
+{
+	y = ((x / 10) + '0');
+	z = ((x % 10) + '0');
+	if ((y != z) && (z > y))
+	{
+		putchar(y);
+		putchar(z);
+		putchar(',');
+	putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
