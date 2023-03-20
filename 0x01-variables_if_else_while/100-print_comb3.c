2335580ad002d1829b369ab278d5267593b780dd
HafsaBLF <Hafsablf@gmail.com>
2023-03-20 15:48:07 -0700

    ADD

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,17 @@ int main(void)
 int x, y, z;
 for (x = 0; x <= 100; x++)
 {
-	y = ((x / 10) + '0');
-	z = ((x % 10) + '0');
-	if ((y != z) && (z > y))
-	{
-		putchar(y);
-		putchar(z);
-		putchar(',');
-	putchar(' ');
+y = ((x / 10) + '0');
+z = ((x % 10) + '0');
+if ((y != z)&&(z > y))
+{
+putchar(y);
+putchar(z);
+if (x != 100)
+{
+putchar(',');
+putchar(' ');
+}
 }
 }
 putchar('\n');
