4ccb509e058b1743c8e66140ad9c44f9d4d58b8f
HafsaBLF <Hafsablf@gmail.com>
2023-03-21 18:57:42 -0700

    ADD

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,14 @@
  */
 int main(void)
 {
-char msg = "_putchar";
-putchar(msg);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
 putchar('\n');
 return (0);
 }
