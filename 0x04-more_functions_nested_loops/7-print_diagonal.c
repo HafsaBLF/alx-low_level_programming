3a277c9f7fa00260d8f996dc414a611993883871
HafsaBLF <Hafsablf@gmai.com>
2023-03-24 17:27:11 -0700

    ADD

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,21 +6,23 @@
  */
 void print_diagonal(int n)
 {
-int i = 0;
-int f = 0;
-while (n > i)
+if (n <= 0)
 {
-if (n > 0)
-{
-while (f <= i - 1)
-{
-_putchar(' ');
-f++;
+_putchar('\n');
 }
+else
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (j == i)
 _putchar('\\');
+else if (j < i)
+_putchar(' ');
+}
 _putchar('\n');
 }
-i++;
 }
-_putchar('\n');
 }
