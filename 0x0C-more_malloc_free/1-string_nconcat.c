6add08633b648df87e1e8efc5e73d9d32f0bdba0
HafsaBLF <Hafsablf@gmai.com>
2023-04-13 17:50:32 -0700

    Add

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ **string_nconcat - concatenate 2 strings
+ *@n: unsigned int
+ *@s2: char
+ *@s1: char
+ *Return: nothing
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+return (0);
+}
