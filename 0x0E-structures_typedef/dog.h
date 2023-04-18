49a0f7bb0e17c4137c90dc5c48871f61cac3d96b
HafsaBLF <Hafsablf@gmai.com>
2023-04-17 17:57:12 -0700

    ADD

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type
+ *@name: pointer
+ *@owner: pointer
+ *@age: nbr
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+dog;
+#endif
