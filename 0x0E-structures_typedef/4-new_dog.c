f094c8c5600fec5b00fa90c5784cad8b6063c402
HafsaBLF <Hafsablf@gmai.com>
2023-04-17 20:00:48 -0700

    ADD

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ *dog_t *new_dog - create a new file
+ *@name: pointer
+ *@age: nbr
+ *@owner: pointer
+ *
+ *return: NULL if false
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog = malloc(sizeof(dog_t))
+		if (new_dog == NULL)
+			return (NULL);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *free_dog - free
+ *@d: pointer
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
