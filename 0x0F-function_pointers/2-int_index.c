5b6c26a689a5448f7528500679799ee14aba94a3
HafsaBLF <Hafsablf@gmai.com>
2023-04-19 16:35:49 -0700

    ADD

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,11 +5,13 @@
  *@array: pointer
  *@cmp: pointer
  *@size: size of array
+ *
+ *Return: the index of the first element
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if (size <= 0)
+if (size <= 0 || array == NULL || cmp == NULL)
 return (-1);
 for (i = 0; i < size; i++)
 if (cmp(array[i]))
