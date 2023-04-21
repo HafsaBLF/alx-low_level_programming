f0abb92bfcd902897eeb27fee3f789f6cc0343aa
HafsaBLF <Hafsablf@gmai.com>
2023-04-20 17:07:23 -0700

    New tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ *print_numbers - print nbr
+ *@n: const
+ *@separator: pointer
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list op;
+unsigned int i;
+int x;
+x = 0;
+	va_start(op, n);
+	if (separator == NULL)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(op, int);
+	printf("%d", x);
+	if (i != (n - 1))
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(op);
+}
