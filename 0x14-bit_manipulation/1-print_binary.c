44bea64effedf508bed319054555bd52d200e0d4
HafsaBLF <Hafsablf@gmai.com>
2023-05-04 17:51:21 -0700

    ADD

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_binary - print the binary
+ *@n: int
+ *Return: binary
+ */
+void print_binary(unsigned long int n)
+{
+int i, x;
+i = (sizeof(n) * 8);
+x = 0;
+
+while (i)
+{
+
+}
+}
